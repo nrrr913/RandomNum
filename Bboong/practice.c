a3c9172946e31a739b8efef40ed96b784edb4d1f
nrrr913 <narin2777@naver.com>
2022-12-20 12:19:09 +0900

    function call

--- Bboong/practice.c
+++ Bboong/practice.c
@@ -5,10 +5,16 @@
 int main() {
 	srand(time(NULL));
 	int QuizNum = 1;
+	for (int i = 1; i <= 10; i++) {
+		Quiz(QuizNum);
+		QuizNum++;
+	}
 	
 }
 
 int Quiz(int QuizNum) {
 	int First = rand() % 20 + 1;
 	int Second = rand() % 20 + 1;
+	int cal;
+	printf("[%d/10] %d + %d = ", QuizNum, First, Second);
 }
