73157b139bad09f6638ed28f98036bb326830796
nrrr913 <narin2777@naver.com>
2022-12-20 12:08:57 +0900

    random function

---
+++ Bboong/practice.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+	srand(time(NULL));
+	int QuizNum = 1;
+	
+}
+
+int Quiz(int QuizNum) {
+	int First = rand() % 20 + 1;
+	int Second = rand() % 20 + 1;
+}
